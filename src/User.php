b2fa47f7cc6132154b6d3aa0d12ac36e570f108a
Owen Doyle <owen66@gmail.com>
2015-12-07 01:17:16 +0000

    Replaced model with Doctrine implementation

---
+++ src/User.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class User
+{
+    protected $id;
+    protected $name;
+    protected $password;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+}
